ba9e1a751836217bf24ec1f4f7f60df1ecbc3279
Tyco van der Steen <tycovds@gmail.com>
2021-03-11 20:02:27 +0100

    helemaal top

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
   {
-    path: '/test',
-    name: 'Test',
-    component: () => import('../views/Test.vue')
+    path: '/',
+    name: 'Home',
+    component: Home
   }
   
 ]
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,181 +1,116 @@
 <template>
-  <div class="home">
-    <reset-state />
-    <half-wrapper name="Bovenste Helft">
-      <ul>
-        <num-input
-          :config="{ half: 'top', key: 'one', name: 'Enen', max: 5, step: 1 }"
-        />
-        <num-input
-          :config="{
-            half: 'top',
-            key: 'two',
-            name: 'Tweeën',
-            max: 10,
-            step: 2,
-          }"
-        />
-        <num-input
-          :config="{
-            half: 'top',
-            key: 'three',
-            name: 'Drieën',
-            max: 15,
-            step: 3,
-          }"
-        />
-        <num-input
-          :config="{
-            half: 'top',
-            key: 'four',
-            name: 'Vieren',
-            max: 20,
-            step: 4,
-          }"
-        />
-        <num-input
-          :config="{
-            half: 'top',
-            key: 'five',
-            name: 'Vijven',
-            max: 25,
-            step: 5,
-          }"
-        />
-        <num-input
-          :config="{
-            half: 'top',
-            key: 'six',
-            name: 'Zessen',
-            max: 30,
-            step: 6,
-          }"
-        />
-        <li>
-          <span>Totaal punten boven</span>
-          <span>{{ this.$store.state.topTotal }}</span>
-        </li>
-        <li>
-          <span>Bonus: {{ this.$store.state.topTotal }} / 63 </span>
-          <span>{{ this.$store.state.bonus }}</span>
-        </li>
-        <li>
-          <span>Totaal boven</span>
-          <span>{{ this.$store.state.topTotal + this.$store.state.bonus }}</span>
-        </li>
-      </ul>
-    </half-wrapper>
-    <half-wrapper name="Onderste Helft">
-      <ul>
-        <num-input
-          :config="{
-            half: 'bottom',
-            key: 'three_of_a_kind',
-            name: 'Three of a kind',
-            max: 30,
-            step: 1,
-          }"
-        />
-        <num-input
-          :config="{
-            half: 'bottom',
-            key: 'carre',
-            name: 'Carré',
-            max: 30,
-            step: 1,
-          }"
-        />
-        <num-input
-          :config="{
-            half: 'bottom',
-            key: 'full_house',
-            name: 'Full House',
-            max: 25,
-            step: 25,
-          }"
-        />
-        <num-input
-          :config="{
-            half: 'bottom',
-            key: 'kleine_straat',
-            name: 'Kleine straat',
-            max: 30,
-            step: 30,
-          }"
-        />
-        <num-input
-          :config="{
-            half: 'bottom',
-            key: 'grote_straat',
-            name: 'Grote straat',
-            max: 40,
-            step: 40,
-          }"
-        />
-        <num-input
-          :config="{
-            half: 'bottom',
-            key: 'yahtzee',
-            name: 'Yahtzee',
-            max: 50,
-            step: 50,
-          }"
-        />
-        <num-input
-          :config="{
-            half: 'bottom',
-            key: 'chance',
-            name: 'Chance',
-            max: 30,
-            step: 1,
-          }"
-        />
-        <li>
-          <span>Totaal onder</span>
-          <span>{{ this.$store.state.bottomTotal }}</span>
-        </li>
-      </ul>
-    </half-wrapper>
-    <half-wrapper name="Totaal generaal">
-      <ul>
-        <li>
-          <span>Totaal onder</span>
-          <span>{{ this.$store.state.bottomTotal }}</span>
-        </li>
-        <li>
-          <span>Totaal boven</span>
-          <span>{{ this.$store.state.topTotal + this.$store.state.bonus  }}</span>
-        </li>
-        <li>
-          <span>Totaal generaal</span>
-          <span>{{ this.$store.state.generalTotal }}</span>
-        </li>
-      </ul>
-    </half-wrapper>
+  <div class="container">
+    <reset />
+    <ul class="scores">
+      <!-- top scores -->
+      <h1>Bovenste Helft</h1>
+      <test-input
+        v-for="(val, key) in topScores"
+        :key="key"
+        :data="{ val, key, half: 'top' }"
+        class="input-wrapper"
+      />
+      <li>
+        <span>Totaal aantal punten</span> <span>{{ totalTop }}</span>
+      </li>
+      <li>
+        <span>Bonus <em v-if="bonus < 35">({{totalTop}} / 63)</em> </span> <span>{{ bonus }}</span>
+      </li>
+      <li>
+        <span>Totaal bovenste helft</span> <span>{{ totalTop + bonus}}</span>
+      </li>
+      <h1>Onderste Helft</h1>
+      <test-input
+        v-for="(val, key) in bottomScores"
+        :key="key"
+        :data="{ val, key, half: 'bottom' }"
+        class="input-wrapper"
+      />
+
+     
+      <!-- score totaal  generaal -->
+      <h1>Generaal totaal</h1>
+       <li>
+        <span>Totaal onderste helft</span> <span>{{ totalBottom }}</span>
+      </li>
+       <li>
+        <span>Totaal bovenste helft</span> <span>{{ totalTop + bonus }}</span>
+      </li>
+       <li>
+        <span>Totaal generaal</span> <span>{{ totalTop + bonus + totalBottom }}</span>
+      </li>
+
+    </ul>
   </div>
 </template>
 
 <script>
-import HalfWrapper from "../components/HalfWrapper.vue";
-import NumInput from "../components/NumberInput.vue";
-import  ResetState from '../components/Reset.vue';
+import TestInput from "../components/ScoreInput.vue";
+import Reset from "../components/Reset";
 export default {
-  name: "Home",
   components: {
-    "half-wrapper": HalfWrapper,
-    "num-input": NumInput,
-    "reset-state": ResetState
+    "test-input": TestInput,
+    Reset,
+  },
+  data() {
+    return {
+      editing: false,
+    };
+  },
+  computed: {
+    topScores() {
+      return this.$store.state.scores.top;
+    },
+    bonus(){
+      return this.totalTop >= 63 ? 35 : 0;
+    },
+    bottomScores() {
+      return this.$store.state.scores.bottom;
+    },
+    totalTop() {
+      return this.calculateTotal(this.$store.state.scores.top);
+    },
+    totalBottom() {
+      return this.calculateTotal(this.$store.state.scores.bottom);
+    },
+  },
+  methods: {
+    calculateTotal(scores) {
+      return Object.values(scores)
+        .map((obj) => {
+          return obj.val;
+        })
+        .reduce((a, b) => {
+          return Number(a) + Number(b);
+        }, 0);
+    },
   },
 };
 </script>
+
 <style lang="scss">
-.home {
+@use '../assets/scss/slider';
+@use '../assets/scss/variables' as v;
+.container {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
-  
-  
+  .scores {
+    padding: 1rem;
+    max-width: 500px;
+    width: 100%;
+  }
+  h1 {
+    margin-bottom: 0.5em;
+    color: #fff;
+    text-shadow: 2px 2px 2px rgba($color: #000000, $alpha: 0.3);
+  }
+  ul {
+    list-style: none;
+    li {
+      @include v.li-tag;
+    }
+  }
 }
 </style>
