1bb7752f1d52a3bf92297648c978680cdba2a8bf
Tyco van der Steen <tycovds@gmail.com>
2021-03-13 15:48:09 +0100

    icons generated

--- vue.config.js
+++ vue.config.js
@@ -5,12 +5,5 @@ module.exports = {
     pwa: {
         name: 'Yahtzee',
         themeColor: '#0038ae',
-        iconPaths: {
-                favicon32: 'img/icons/favicon-32.png',
-                favicon16: 'img/icons/chrome-favicon-16-16.png',
-                appleTouchIcon: 'img/icons/phone-150-150.png',
-                maskIcon: null,
-                msTileImage: 'img/icons/msTile-144-144.png'
-        }
     }
 }
