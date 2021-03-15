6238b30ea79d31ec0cc9c892029aeb380aa2990c
Tyco van der Steen <tycovds@gmail.com>
2021-03-15 13:15:36 +0100

    new transition effect

--- src/assets/globalFunctions.js
+++ src/assets/globalFunctions.js
@@ -1,13 +1,27 @@
+const duration = 75;
 export function listTransition() {
     const listItems = document.querySelectorAll(".scores li");
     listItems.forEach((li, i) => {
-        li.style.transform = "translateX(50px)";
+        li.style.transform = "rotateX(90deg)";
         li.style.opacity = "0";
         li.style.transformProperty = "transform opacity";
         li.style.transition = "200ms ease";
         setTimeout(() => {
-            li.style.transform = "translateX(0)";
+            li.style.transform = "rotateX(0deg)";
             li.style.opacity = "1";
-        }, i * 75);
+        }, i * duration + duration);
     });
 }
+// export function listTransition() {
+//     const listItems = document.querySelectorAll(".scores li");
+//     listItems.forEach((li, i) => {
+//         li.style.transform = "translateX(50px)";
+//         li.style.opacity = "0";
+//         li.style.transformProperty = "transform opacity";
+//         li.style.transition = "200ms ease";
+//         setTimeout(() => {
+//             li.style.transform = "translateX(0)";
+//             li.style.opacity = "1";
+//         }, i * 75);
+//     });
+// }
